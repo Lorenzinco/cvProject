a2b71c69e0c6b4d48d48d35da56bb25d06c6e6e5
Lorenzo Colombini <lorenzinco23@gmail.com>
2025-01-09 15:24:43 +0100

    updated the library for the menu

--- utils/choose_camera.py
+++ utils/choose_camera.py
@@ -1,7 +1,7 @@
 import cv2
-from simple_term_menu import TerminalMenu
+from pick import pick
 
-def returnCameraIndexes():
+def returnCameras():
     # checks the first 10 indexes.
     index = 0
     arr = []
@@ -9,14 +9,14 @@ def returnCameraIndexes():
     while i > 0:
         cap = cv2.VideoCapture(index)
         if cap.read()[0]:
-            arr.append(index)
+            arr.append("Camera ("+str(index)+")")
             cap.release()
         index += 1
         i -= 1
     return arr
 
 def choose_camera():
-	indexes = returnCameraIndexes()
-	menu = TerminalMenu(["Camera " + str(i) for i in indexes])
-	menu_entry_index = menu.show()
-	return indexes[menu_entry_index]
+	options = returnCameras()
+	title = 'Please choose a Camera from the cameras avaliable below:'
+	camera_index, index = pick(options,title)
+	return index
